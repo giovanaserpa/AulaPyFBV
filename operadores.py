b7d51f0cf5fd4ff2a351cc77e3971f778aee982b
Giovana Serpa <gel_serpa@hotmail.com>
2025-08-19 09:57:16 -0300

    Aula dia 19/09 UNIFBV

---
+++ operadores.py
@@ -0,0 +1,30 @@
+a=10
+b=3
+
+#Adição
+soma = a+b
+print('Adição:', soma)
+
+#Subtração
+subtracao = a-b
+print('Subtração:', subtracao)
+
+#Multiplicação
+multiplicacao = a*b
+print('Multiplicação:', multiplicacao)
+
+#Divisão (float)
+divisao = a/b
+print('Divisão:', divisao)
+
+#Divisão inteira
+divisao_inteira = a // b
+print('Divisão inteira:', divisao_inteira)
+
+#Módulo (resto da divisão)
+modulo = a%b
+print('Modulo:', modulo)
+
+#Exponenciação (potência)
+potencia = a**b
+print('Potência:', potencia)
