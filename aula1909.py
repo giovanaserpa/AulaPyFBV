316847d1825473a75e78ad00865867c15b5c6595
Giovana Serpa <gel_serpa@hotmail.com>
2025-08-19 14:14:44 -0300

    Update aula1909.py

--- aula1909.py
+++ aula1909.py
@@ -1,12 +1,13 @@
-#nome=input('digite seu nome:')
-#idade=int(input('digite sua idade:'))
-#faculdade='unifbv'
-#print(f'seu nome é {nome} e estuda na faculdade {faculdade}')
-#print(f'sua idade daqui a 5 anos é {idade+5}')
-#print('seu nome é', nome,'e estuda na faculdade',faculdade)
-#print('seu nome é +'nome+' e estuda na faculdade' +faculdade)
+nome=input('digite seu nome:')
+idade=int(input('digite sua idade:'))
+faculdade='unifbv'
+print(f'seu nome é {nome} e estuda na faculdade {faculdade}')
+print(f'sua idade daqui a 5 anos é {idade+5}')
+print('seu nome é', nome,'e estuda na faculdade',faculdade)
+print('seu nome é +'nome+' e estuda na faculdade' +faculdade)
 
 num1 = float(input('Digite um número:'))
 num2 = float(input('Digite outro número:'))
 soma = num1+num2
+
 print('A soma entre', num1, 'e', num2, 'é', soma)
