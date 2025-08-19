1a64b77aaa3d6b91d09c18415a584647265b6436
Giovana Serpa <gel_serpa@hotmail.com>
2025-08-19 10:15:20 -0300

    Exercícios SAVA

---
+++ exerc.py
@@ -0,0 +1,17 @@
+#Exercício 1
+nome=input('Qual o seu nome?')
+cidade=input('Qual a sua cidade?')
+print(f'Bem vindo', nome, 'como está o dia em', cidade,'?')
+
+#Exercício 2
+num1=int(input('Digite um número:'))
+num2=int(input('Digite outro número:'))
+num3=int(input('Digite outro número:'))
+soma = num1+num3
+multi= num1*num2
+print('A soma entre o número 1 e 3 é', soma, 'e a multiplicação entre o número 1 e 2 é', multi,)
+
+#Exercício 3
+graus=float(input('Graus em celsius:'))
+f=(graus*1.8)+32
+print(graus, 'Cº é igual a', f, 'Fº')
